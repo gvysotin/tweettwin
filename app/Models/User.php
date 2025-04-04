2e6af881b7f28411047dbaa35217c604e8ead775
gvysotin <spideful@yandex.ru>
2025-04-04 12:12:05 +0800

    Addition comments

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,11 +60,13 @@ class User extends Authenticatable
         return $this->hasMany(Comment::class)->latest();
     }
 
+    // Этот метод возвращает коллекцию пользователей, за которыми подписан текущий пользователь.
     public function followings()
     {
         return $this->belongsToMany(User::class,'follower_user', 'follower_id', 'user_id')->withTimestamps();
     }
 
+    // Этот метод возвращает коллекцию пользователей, которые подписаны на текущего пользователя.
     public function followers()
     {
         return $this->belongsToMany(User::class,'follower_user', 'user_id', 'follower_id')->withTimestamps();
